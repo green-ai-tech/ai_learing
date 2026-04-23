137f2259e321b17416d5d1f553cb95f71212946f
green-ai-tech <kenjenkins1972@gmail.com>
2026-04-23 14:58:06 +0800

    2026/04/23

--- notebooks/04_LLM_agent/MCP/mcpagent.py
+++ notebooks/04_LLM_agent/MCP/mcpagent.py
@@ -1,27 +1,38 @@
-import asyncio    #异步io
+import asyncio   # 异步io
 from langchain.agents import create_agent
+
 from langchain_mcp_adapters.client import MultiServerMCPClient
 
 client = MultiServerMCPClient({
     "math":{
-        "transport":"stdio",
-        "command":"python",
-        "args":["/Users/logicye/Code/ai_learning/notebooks/04_大模型agent/MCP/mcpserver.py",]
-    }
+        "transport": "stdio",
+        "command": "python",
+        "args": ["/Users/logicye/Code/ai_learning/notebooks/04_LLM_agent/MCP/mcpserver.py", ]   # 路径
+    },
 })
 
-#查询可用工具
-tools = asyncio.run(client.get_tools())
-
+# 查询可用工具
+tools = asyncio.run(client.get_tools())    # 异步操作，需要同步等待
+# tools = client.get_tools()
 print(tools)
 
+# 使用工具创建代理
 
-#使用工具创建代理·
+from langchain_ollama import ChatOllama
+llm = ChatOllama(
+    model="qwen3.6:latest",
+    base_url="http://192.168.8.21:11434"  
+)
+agent = create_agent(
+    model=llm,
+    tools=tools
+)
 
-agent = create_agent.ai({
+math_response = asyncio.run(agent.ainvoke({
     "messages":[
-        "role":"user",
-        "content":"3加5，再乘以10，等于多少？"
+        {"role": "user", "content": "3加5，再乘以10，等于多少"}
     ]
-    
-})
+}))
+
+# print(math_response["messages"][-1].content)
+print(math_response)
--- notebooks/04_LLM_agent/MCP/mcpserver.py
+++ notebooks/04_LLM_agent/MCP/mcpserver.py
@@ -1,29 +1,24 @@
-#MCP:Model Context Protocol
-#环境：pip install fastmcp
+# MCP = Model Context Protocol
+# 环境: pip install fastmcp
+
 from fastmcp import FastMCP
 
-mcp = FastMCP("Math")   
+mcp = FastMCP("Math") # 类似HTTP响应对象，但不准确
 
 @mcp.tool
-def add(a:int,b:int)->int:
-    """将a与b相加,返回和"""
-    return a+b
-
+def add(a: int, b: int) -> int:
+    """
+    执行加法运算，将`a`和`b`相加
+    """
+    return a + b
 
 @mcp.tool
-def mulity(a:int,b:int)->int:
-    """将a与b相乘,返回和"""
-    return a*b
+def multy(a: int, b: int) -> int:
+    """
+    执行乘法运算，将`a`和`b`相乘
+    """
+    return a * b
 
-
-#发布服务
-
-if __name__ == "__mian__":
-    mcp.run()
-
-
-
-
-
-
-
+# 发布成服务
+if __name__=="__main__":
+    mcp.run(transport="stdio")   # 默认stdio  # 本地进程
